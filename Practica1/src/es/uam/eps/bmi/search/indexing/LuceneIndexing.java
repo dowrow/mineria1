1206f53c49398607158afff96a478206064db608
Dowrow <dowrow@outlook.com>
2015-01-31 17:34:03 +0100

    Clases vacías

---
+++ Practica1/src/es/uam/eps/bmi/search/indexing/LuceneIndexing.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.uam.eps.bmi.search.indexing;
+
+import es.uam.eps.bmi.search.TextDocument;
+import es.uam.eps.bmi.search.parsing.TextParser;
+import java.util.List;
+
+public class LuceneIndexing implements Index {
+
+    @Override
+    public void build(String inputCollectionPath, String outputIndexPath, TextParser textParser) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void load(String indexPath) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<String> getDocIds() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public TextDocument getDocument(String docId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<String> getTerms() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Posting> getTermsPosting(String term) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
