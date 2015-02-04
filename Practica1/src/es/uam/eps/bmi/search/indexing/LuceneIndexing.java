abd34d8140f47a19407d6b6cbb4ffe94341caa52
eps <eps@150.244.65.172>
2015-02-04 18:41:42 +0100

    LuceneIndexing my ass

--- Practica1/src/es/uam/eps/bmi/search/indexing/LuceneIndexing.java
+++ Practica1/src/es/uam/eps/bmi/search/indexing/LuceneIndexing.java
@@ -1,18 +1,31 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package es.uam.eps.bmi.search.indexing;
 
 import es.uam.eps.bmi.search.TextDocument;
 import es.uam.eps.bmi.search.parsing.TextParser;
+import java.io.File;
+import java.io.FileInputStream;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 public class LuceneIndexing implements Index {
 
     @Override
     public void build(String inputCollectionPath, String outputIndexPath, TextParser textParser) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            File fichero = new File(inputCollectionPath);
+            ZipInputStream zis = new ZipInputStream(new FileInputStream(fichero));
+            ZipEntry entry = null;
+            
+            while ((entry = zis.getNextEntry()) != null) {
+                System.out.println(entry.getName());                
+            }
+            
+        } catch (Exception e) {
+            
+        }
+        
     }
 
     @Override
@@ -40,4 +53,9 @@ public class LuceneIndexing implements Index {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
+    public static void main (String args[]) {
+        LuceneIndexing li = new LuceneIndexing();
+        TextParser tp = null;
+        li.build("ficherin", "index", tp);
+    }
 }
