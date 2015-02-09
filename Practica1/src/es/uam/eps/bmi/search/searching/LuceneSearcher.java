7c9997ff1aacf8b05b2994111cba6182122571bd
garnachod <garnachod@gmail.com>
2015-02-09 12:25:33 +0100

    getDocIds de LuceneIndexing creo que no se hace asi XD, habria que usar la solucion buena para luceneSearcher.

--- Practica1/src/es/uam/eps/bmi/search/searching/LuceneSearcher.java
+++ Practica1/src/es/uam/eps/bmi/search/searching/LuceneSearcher.java
@@ -3,18 +3,68 @@ package es.uam.eps.bmi.search.searching;
 
 import es.uam.eps.bmi.search.ScoredTextDocument;
 import es.uam.eps.bmi.search.indexing.Index;
+import es.uam.eps.bmi.search.indexing.LuceneIndexing;
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.queryParser.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.SimpleFSDirectory;
+import org.apache.lucene.util.Version;
 
 public class LuceneSearcher implements Searcher {
-
+    // Inspeccionar el índice
+    private IndexReader ireader = null;
+    private QueryParser qParser = null;
+    private IndexSearcher isearcher = null;
     @Override
     public void build(Index index) {
+        
+        try{
+            
+            //apertura del indice
+            Directory directory = new SimpleFSDirectory(new File(index.getPath()));
+            this.ireader = IndexReader.open(directory);
+            this.isearcher = new IndexSearcher(this.ireader);
+            //query parser, parsea igual que el creador de indices.
+            Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_31);
+            this.qParser = new QueryParser(Version.LUCENE_36, "content", analyzer);
+            
+        }catch(Exception e){
+             Logger.getLogger(LuceneSearcher.class.getName()).log(Level.SEVERE, null, e);
+        }
+        
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public List<ScoredTextDocument> search(String query) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ArrayList<ScoredTextDocument> scoredTDList = new ArrayList<>();
+        try{
+            Query q = this.qParser.parse(query);
+            int topDocs = 10;
+            ScoreDoc result[] = this.isearcher.search(q, topDocs).scoreDocs;
+            
+            for(int i = 0; i < result.length; i++){
+                //hacer un cambio entre identificador de documento 
+                //a String del documento
+                
+                ScoredTextDocument sctd = new ScoredTextDocument(result[i].doc+"" , result[i].score);
+                scoredTDList.add(sctd);
+            }
+            
+        }catch(Exception e){
+            Logger.getLogger(LuceneSearcher.class.getName()).log(Level.SEVERE, null, e);
+        }
+        return scoredTDList;
     }
     
 }
--- Practica1/src/es/uam/eps/bmi/search/searching/Searcher.java
+++ Practica1/src/es/uam/eps/bmi/search/searching/Searcher.java
@@ -6,6 +6,17 @@ import es.uam.eps.bmi.search.indexing.Index;
 import java.util.List;
 
 public interface Searcher {
+    /**
+     * crea el buscador a partir del índice pasado como argumento de entrada
+     * @param index índice con el que crear el buscador
+     */
     void build (Index index);
+    /**
+     * que devolverá un ranking (ordenado por score decreciente) de documentos, 
+     * resultantes de ejecutar una consultada dada sobre el índice del buscador.
+     * 
+     * @param query Consulta a ejecutar
+     * @return ranking (ordenado por score decreciente) de documentos
+     */
     List<ScoredTextDocument> search (String query);
 }
